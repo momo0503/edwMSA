4229bb69755e499ce682d69f256b55557eea0914
momo0503 <yiyi6464@naver.com>
2022-03-13 16:56:04 +0900

    2022-03-13

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -4,9 +4,7 @@ import com.example.userservice.vo.Greeting;
 import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
@@ -31,5 +29,10 @@ public class UserController {
     @GetMapping("/welcome")
     public String welcome(){
         return env.getProperty("greeting.message");
+    }
+
+    @PostMapping("/users")
+    public String createUser(@RequestBody RequestUser user){
+        return "Create user method is called";
     }
 }
